4d811f239f20c265f30e65bdbc5c17eede4205ff
Michał <bodysmic@gmail.com>
2022-12-13 19:19:29 +0100

    negative values test

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -15,6 +15,13 @@ import { formatAmountInCurrency } from '../../utils/formatAmountInCurrency';
 
   ]
 
+  const negativeTestCases=[
+    {amount: -100, from: 'PLN', to: 'USD'},
+    {amount: -85, from: 'PLN', to: 'USD'},
+    {amount: -10, from: 'USD', to: 'PLN'},
+    {amount: -11, from: 'USD', to: 'PLN'},
+  ]
+
 
      it('should render without crashing', () => {
     render (<ResultBox from='PLN' to='USD' amount={100} />);
@@ -38,8 +45,16 @@ import { formatAmountInCurrency } from '../../utils/formatAmountInCurrency';
   it('should render equal info when PLN ->' ,() => {
     for(const testCase of testCases){
       render(<ResultBox from='PLN' to='PLN' amount={testCase.amount}/>);
-      const output = screen.getByTestId('main-div');
+      const output = screen.getByTestId('main-div')
       expect(output).toHaveTextContent(`${formatAmountInCurrency(testCase.amount, 'PLN')} = ${formatAmountInCurrency(testCase.amount, 'PLN')}`)
+      cleanup()
+    }
+  })
+  it('should render "Wrong value..." when value is lower then 0', () => {
+    for(const negativeTestCase of negativeTestCases){
+      render(<ResultBox from={negativeTestCase.from} to={negativeTestCase.to} amount={negativeTestCase.amount}/>);
+      const output = screen.getByTestId('main-div');
+      expect(output).toHaveTextContent(`Wrong value...`)
       cleanup()
     }
   })
